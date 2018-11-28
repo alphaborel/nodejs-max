54cc87c46a9ba503c2c2d4f874ac3b9ba0303095
Tony B <aaborel@gmail.com>
2018-11-28 12:44:30 -0700

    Serving HTML pages

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middlewere')
-    
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(shopRoutes);
+app.use('/admin/', adminRoutes);
+
+// 404 page
+app.use('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
